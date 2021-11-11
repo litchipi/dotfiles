1498d9dd4fe426d60bf5fff14bc517d3eab0282d
Litchi Pi <litchi.pi@protonmail.com>
2021-11-11 18:37:58 +0100

    11/11/21 18:37:58 | Backing up dotfiles of machine 'sparta'
    Signed-off-by: Litchi Pi <litchi.pi@protonmail.com>

--- global/aliases.sh
+++ global/aliases.sh
@@ -20,6 +20,31 @@ alias grep='grep --color=auto'
 # Network-related
 alias pingt='ping -c 1 www.google.fr 1> /dev/null 2> /dev/null && echo -e "'${GREEN_BOLD}'Connected'${RESET}'" || echo -e "'${RED_BOLD}'No connection'${RESET}'"'
 
+# VPN
+alias vpn_logout='protonvpn-cli logout'
+alias vpn_login='/usr/bin/expect ~/.protonvpn_login.tcl litchi.pi $(gpg -q -d ~/.vpn_creds.gpg)'
+alias vpn='protonvpn-cli c --sc'
+alias vpn_p2p='protonvpn-cli c --p2p'
+alias vpn_tor='protonvpn-cli c --tor'
+alias vpn_cc='protonvpn-cli c --cc'
+alias vpn_random='protonvpn-cli c -r'
+alias vpn_fast='protonvpn-cli c -f'
+alias novpn='protonvpn-cli d'
+echo '#!/usr/bin/expect
+set user [lindex $argv 0]
+set p [lindex $argv 1]
+spawn -noecho protonvpn-cli login $user
+expect {
+    "already logged" { exit 0 };
+    "password" {
+        send_user "Entering password";
+        send "$p\n";
+        interact;
+        exit 0;
+    };
+    timeout { send_user "Timeout"; exit 254 };
+    eof { exit 253 };
+};' > $HOME/.protonvpn_login.tcl
 
 # Jrnl
 alias djrnl='__djrnl'
@@ -43,6 +68,16 @@ source $HOME/.git-completion
 alias ga="git add ."
 alias gcm="git commit -s -m"
 alias gts="git status"
+alias gitclean="git fetch -p && git gc --prune=now"
+
+alias gb="git branch --list"
+alias gba="git branch --list --all"
+alias gbc="git checkout -B "
+alias gbd="git branch -D"
+__git_complete gbd _git_branch
+
+alias gck="git checkout"
+__git_complete gck _git_checkout
 
 alias gl="git log --oneline -30"
 __git_complete gl git_log
@@ -61,7 +96,11 @@ __git_complete gcp  _git_cherry_pick
 
 alias gpsh="git push"
 __git_complete gpsh _git_push
-alias gkeep='git branch tmp.$(git rev-parse --short HEAD)'
+
+function __get_current_branch() {
+	git branch |grep "*"| cut -d " " -f 2
+}
+alias gkeep='git branch $(__get_current_branch).$(git rev-parse --short HEAD)'
 
 alias gg="git gui &"
 alias gitka="gitk --all --max-count=5000"
@@ -69,7 +108,73 @@ alias ggk="git gui & gitk &"
 alias ggka="git gui & gitka &"
 
 
+alias gsave="__gsave"
+function __gsave() {
+	if [ $# -ne 1 ] ; then
+		echo "Usage: $0 <branch name>. Will create a temporary commit and checkout to the given branch"
+		exit 1;
+	fi
+	set -e
+	git add .
+	git commit -m "TMP COMMIT"
+	git checkout $1
+}
+__git_complete gsave _git_checkout
 
+alias gload="__gload"
+function __gload() {
+	if [ $# -ne 1 ] ; then
+		echo "Usage: $0 <branch name>. Will create a temporary commit and checkout to the given branch"
+		exit 1;
+	fi
+	set -e
+	git checkout $1
+	git reset --mixed HEAD~1
+}
+__git_complete gload _git_checkout
+
+ALIAS_COLOR="\033[1;96m"
+RESET="\033[0m"
+function __gh() {
+	echo -e "$ALIAS_COLOR $1:   $RESET $2"
+}
+
+function __ghelp() {
+	echo "Aliases defined for git:"
+	echo ""
+	__gh "ga" "Add everything to repo"
+	__gh "gcm" "Commit signed with a message (arg: message)"
+	__gh "gts" "Get status of repository"
+	__gh "gpsh" "Push the repository to remote"
+	__gh "gitclean" "Clean the repository, garbage collect, prune"
+	echo ""
+	__gh "gb" "List the local branches"
+	__gh "gba" "List all the branches"
+	__gh "gbc" "Create a new branch if doesn't exist, checkout on it"
+	__gh "gbd" "Delete a branch"
+	__gh "gck" "Checkout to a branch"
+	echo ""
+	__gh "gl" "Commit log of current branch"
+	__gh "gla" "Commit log of every branches"
+	__gh "gdf" "Print the diff since last commit"
+	__gh "grs" "Remove file from staged state"
+	echo ""
+	__gh "grh" "Reset HARD the branch to a given ref (default: HEAD)"
+	__gh "grm" "Reset MIXED the branch to a given ref (default: HEAD)"
+	__gh "gcp" "Cherry pick a commit from the given ref"
+	echo ""
+	__gh "gg" "Starts git gui in the background"
+	__gh "ggk" "Starts git gui with gitk, both in background"
+	__gh "gitka" "Starts gitk for with all branches displayed"
+	__gh "ggka" "Starts git gui with gitka, both in background"
+	echo ""
+	__gh "gkeep" " Create a temporary branch on the given commit to keep the ref"
+	__gh "gsave" "Create a temporary commit with the ongoing changes, and checkout the given branch"
+	__gh "gload" "Checkout the given branch, and restore the last commit made into ongoing changes"
+	echo ""
+}
+
+alias ghelp="__ghelp"
 
 # Youtube-dl
 if [ -d $HOME/Musique ]; then
@@ -89,7 +194,7 @@ alias dl_search_mp3_hq="dl_mp3_hq --default-search \"ytsearch\""
 
 alias dl_flac="youtube-dl $YTDL_ARGS --audio-format flac"
 alias dl_flac_hq="dl_flac --audio-quality 0"
-alias dl_search_mp3="dl_flac --default-search \"ytsearch\""
+alias dl_search_flac="dl_flac --default-search \"ytsearch\""
 alias dl_search_flac_hq="dl_flac_hq --default-search \"ytsearch\""
 
 # defaults
@@ -102,7 +207,7 @@ alias webtopdf="wkhtmltopdf"
 
 
 # Backup
-alias backup="memory all; cd ~/.backup/; make -j$(nproc); cd -"
+alias backup="memory all && cd ~/.backup/ && make -j$(nproc) && cd -"
 
 
 # Nix aliases
