d92f46ea870a98b89b1b51803fed02c97cb32139
Litchi Pi <litchi.pi@protonmail.com>
2021-11-15 12:51:46 +0100

    Adding auto repo clean after push
    Signed-off-by: Litchi Pi <litchi.pi@protonmail.com>

--- push.sh
+++ push.sh
@@ -2,4 +2,5 @@
 
 git add global/ machines/ 1>/dev/null 2>/dev/null
 git commit --signoff -m "$(date "+%d/%m/%y %H:%M:%S") | Backing up dotfiles of machine '$HOSTNAME'" 1>/dev/null 2>/dev/null
-git push
+git push 1>/dev/null 2>/dev/null
+git gc --prune=now 1>/dev/null 2>/dev/null
