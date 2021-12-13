a75cdf1d287f216301c53235e32b99a004cec622
Litchi Pi <litchi.pi@protonmail.com>
2021-12-13 15:04:29 +0100

    Modifying 'push.sh' to allow multiple push
    Signed-off-by: Litchi Pi <litchi.pi@protonmail.com>

--- push.sh
+++ push.sh
@@ -2,5 +2,7 @@
 
 git add global/ machines/ 1>/dev/null 2>/dev/null
 git commit --signoff -m "$(date "+%d/%m/%y %H:%M:%S") | Backing up dotfiles of machine '$HOSTNAME'" 1>/dev/null 2>/dev/null
-git push 1>/dev/null 2>/dev/null
+for remote in $(git remote); do
+    git push $remote 1>/dev/null 2>/dev/null
+done
 git gc --prune=now 1>/dev/null 2>/dev/null
